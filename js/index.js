86f11029a2aa7fc3e8b65620f06d554895fc3c2c
sukanta-ciem <sukanta.ciem@gmail.com>
2018-05-05 11:43:32 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -43,21 +43,21 @@ var site_url = "http://www.arishbionaturals.com/attendance/";
     // current GPS coordinates
     //
     var onSuccess = function(position) {
-        //alert('Latitude: '          + position.coords.latitude          + '\n' +
+        /*alert('Latitude: '          + position.coords.latitude          + '\n' +
               'Longitude: '         + position.coords.longitude         + '\n' +
               'Altitude: '          + position.coords.altitude          + '\n' +
               'Accuracy: '          + position.coords.accuracy          + '\n' +
               'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
               'Heading: '           + position.coords.heading           + '\n' +
               'Speed: '             + position.coords.speed             + '\n' +
-              'Timestamp: '         + position.timestamp                + '\n');
+              'Timestamp: '         + position.timestamp                + '\n');*/
     }
 
     // onError Callback receives a PositionError object
     //
     function onError(error) {
-        //alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
+        /*alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');*/
     }
 
     
